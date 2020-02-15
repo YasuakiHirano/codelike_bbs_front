564c907d3e888fb363fa1f3013ec72bd4ee93daf
y.hirano <yasuakihappy@gmail.com>
2020-02-15 11:48:23 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i" :to="item.to">
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" color="indigo blue" dark fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }} codelike</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'トップ',
+          to: '/'
+        },
+        {
+            icon: 'mdi-pencil',
+            title: '新規投稿',
+            to: '/board'
+        }
+      ],
+      title: 'codelike bbs'
+
+    }
+  }
+}
+</script>
---
+++ pages/board.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-layout>
+    <v-row justify="center">
+      <v-col cols="10">
+        <v-form ref="form" lazy-validation>
+          <v-text-field v-model="title" label="タイトル" required></v-text-field>
+          <v-text-field v-model="name" label="名前" required></v-text-field>
+          <v-textarea v-model="description" label="説明文" required></v-textarea>
+          <v-text-field v-model="password" type="password" label="パスワード" required></v-text-field>
+          <v-row justify="center">
+            <v-col cols="4">
+              <v-btn color="secondary" block @click="reset">
+                <span class="white--text">クリア</span>
+              </v-btn>
+            </v-col>
+            <v-col cols="4">
+              <v-btn color="primary" block @click="reset">
+                <span class="white--text">登録</span>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+      description: '',
+      password: '',
+      name: ''
+    }
+  },
+  methods: {
+    reset () {
+      this.$refs.form.reset()
+    },
+  },
+}
+</script>
